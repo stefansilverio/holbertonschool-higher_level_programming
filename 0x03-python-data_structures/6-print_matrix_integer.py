84cd30c15a3938a0dd4093e25adaef8691e16b6a
stefansilverio <494@holbertonschool.com>
2018-12-12 00:54:46 +0000

    print matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,11 +5,11 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for elem in matrix[pos]:
             if count == 2:
-                print(elem, end='')
+                print("{:d}".format(elem), end='')
                 count += 1
                 continue
             count += 1
-            print(elem, end=' ')
+            print("{:d}".format(elem), end=' ')
         pos += 1
         count = 0
         print()
