317cddbf382863be609b1df8bdea245567edab4f
stefansilverio <494@holbertonschool.com>
2018-12-12 00:52:03 +0000

    print matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    pos = 0
+    count = 0
+    for i in matrix:
+        for elem in matrix[pos]:
+            if count == 2:
+                print(elem, end='')
+                count += 1
+                continue
+            count += 1
+            print(elem, end=' ')
+        pos += 1
+        count = 0
+        print()
