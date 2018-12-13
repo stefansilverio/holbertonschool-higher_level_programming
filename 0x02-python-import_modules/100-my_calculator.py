c883f640bb8deac8ec2e93ac21c30d3d7d2644ac
stefansilverio <494@holbertonschool.com>
2018-12-13 04:54:05 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import calculator_1
+import sys
+if __name__ == "__main__":
+    list = ['+', '-', '/', '%']
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    for i in list:
+        if i == sys.argv[2]:
+            print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[1]) + int(sys.argv[3])))
+            exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
