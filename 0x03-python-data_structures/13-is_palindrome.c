3c0669d3c5d34c8b422b79f5cdb81597b923ea78
stefansilverio <494@holbertonschool.com>
2018-12-13 01:18:58 +0000

    check if list is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,63 +5,36 @@
 /**
  *
  */
-char *helper(unsigned int count, unsigned int position, char int_array[], listint_t *curr);
-int array_palindrome(int *int_array); /* determined if array returned is palindrome*/
-
 int is_palindrome(listint_t **head)
 {
-	unsigned int count = 0;
-	unsigned int position = 0;
-	listint_t *curr = *head;
-	int *int_array;
+	listint_t *curr = *head, *start = *head; /* count nodes */
 
-	while (curr->next)
-	{
-		count++;
-		curr = curr->next;
-	}
+	if ((*head == NULL) || (*head)->next == NULL)
+		return (1);
 
-	curr = *head;
-
-	int_array = helper(count, position, int_array, curr);
-	return(array_palindrome(*int_array, count));
+	return(helper(curr, &start));
 }
 
-char *helper(unsigned int count, unsigned int position, char int_array[], listint_t *curr)
+int helper(listint_t *curr, listint_t **start)
 {
 	if (curr->next) /* recursive case */
 	{
-		count += 1;
-		curr = curr->next;
-		helper(count, int_array, curr);
+		if (!helper(curr->next, start))
+			return (0);
+		printf("1: here:%d\n", curr->n);
+		printf("1: there:%d\n", (*start)->n);
+		if (curr->n != (*start)->n)
+			return (0);
+		*start = (*start)->next;
 	}
 	else
 	{
-		int_array[position] = curr->n;
-		position += 1;
-	}
-	return (int_array);
-}
-
-int array_palindrome(int *int_array) /* determined if array returned is palindrome */
-{
-	int first = 0; /* first position in array */
-	int split= count / 2;
-
-	if (int_array[first] != int_array[count])
-		return (0);
-	else
-	{
-		while (split--)
-		{
-			if (int_array[first] == int_array[count])
-			{
-				count--;
-				first++;
-			}
-			else
-				return (0);
-		}
+		printf("2:here:%d\n", curr->n);
+		printf("2:there%d\n", (*start)->n);
+		if ((*start)->n != curr->n)
+			return (0);
+		*start = (*start)->next;
+		return (1);
 	}
 	return (1);
 }
