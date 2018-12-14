352ae724c5750c1179049c909b5e2880fd997302
stefansilverio <494@holbertonschool.com>
2018-12-14 03:18:12 +0000

    key of best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        if a_dictionary is not None:
-            if len(a_dictionary.items()) != 0:
-                arbys = [(key, value) for key, value in a_dictionary.items()]
-            return(max(arbys)[0])
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
+    arbys = [(key, value) for key, value in a_dictionary.items()]
+    return(max(arbys)[0])
