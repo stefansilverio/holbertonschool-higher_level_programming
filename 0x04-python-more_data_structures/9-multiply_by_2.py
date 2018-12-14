888b8ff332dcc461179a80327c58828195dd5431
stefansilverio <494@holbertonschool.com>
2018-12-14 02:44:45 +0000

    multiply numbers by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if isinstance(a_dictionary, dict):
-        if a_dictionary != None:
+        if a_dictionary is not None:
             new_dict = a_dictionary.copy()
             [new_dict.update((x, y*2) for x, y in new_dict.items())]
         return new_dict
