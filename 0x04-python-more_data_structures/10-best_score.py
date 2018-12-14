55c4d1410831dac3a01a39b8444ceefd77ed79ad
stefansilverio <494@holbertonschool.com>
2018-12-14 02:53:21 +0000

    find key with best value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
         if a_dictionary is not None:
-            reverse = [(value, key) for key, value in a_dictionary.items()]
-            return(max(reverse)[1])
+            reverse = [(key, value) for key, value in a_dictionary.items()]
+            if max(reverse)[0] == 0:
+                return None
+            return(max(reverse)[0])
         return None
