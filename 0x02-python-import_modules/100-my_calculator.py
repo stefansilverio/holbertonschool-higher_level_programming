0b128f2457a26a89d2e7a4865e55909967c87467
stefansilverio <494@holbertonschool.com>
2018-12-14 22:36:36 +0000

    build calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,16 @@ if __name__ == "__main__":
     import sys
     a_dict = {'+': add, '-': sub, '/': div, '*': mul}
 
-    x = int(sys.argv[1])
-    y = int(sys.argv[3])
-
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    elif a_dict.get(sys.argv[2], 0) == 0:
+    if a_dict.get(sys.argv[2], 0) == 0:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    else:
-        print("{} {} {} = {}".format(x, sys.argv[2], y,
-                                     a_dict[sys.argv[2]](x, y)))
-        sys.exit(0)
+    x = int(sys.argv[1])
+    y = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(x, sys.argv[2], y,
+                                 a_dict[sys.argv[2]](x, y)))
