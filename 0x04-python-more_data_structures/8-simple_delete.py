134696cfd5c301400b54e08caf3088e3d5c4316c
stefansilverio <494@holbertonschool.com>
2018-12-14 02:43:42 +0000

    delete key in dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if isinstance(a_dictionary, dict):
-        if a_dictionary != None:
+        if a_dictionary is not None:
             try:
                 del a_dictionary[key]
             except KeyError:
