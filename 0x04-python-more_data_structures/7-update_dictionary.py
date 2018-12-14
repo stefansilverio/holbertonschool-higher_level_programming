641dc20d0c0e9eba3dd59d7af006d16efe5b5a66
stefansilverio <494@holbertonschool.com>
2018-12-14 00:31:40 +0000

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary:
+    if isinstance(a_dictionary, dict):
         a_dictionary[key] = value
-    return a_dictionary
+        return a_dictionary
