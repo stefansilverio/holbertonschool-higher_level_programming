029cf5f67099cec0adb089745a07dbd57c583f1e
stefansilverio <494@holbertonschool.com>
2018-12-17 01:10:37 +0000

    convert roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == True or str == None:
-        roman_dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-        val_list = []
-        for i in roman_string:
-            if i not in roman_dict.keys():
-                return 0
+
+    if isinstance(roman_string, str) is True or str == "None":
+
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                      'D': 500, 'M': 1000}
+        total = 0
+        length = len(roman_string)
+        for i in range(length):
+            if i + 1 == length:
+                total += roman_dict[roman_string[i]]
+                return total
+            elif roman_dict[roman_string[i + 1]] > roman_dict[roman_string[i]]:
+                total += ((-1) * (roman_dict[roman_string[i]]))
             else:
-                val_list.append(roman_dict.get(i))
-        return(sum(val_list))
+                total += roman_dict[roman_string[i]]
+        return total
+
     else:
-        return (0)
+        return 0
