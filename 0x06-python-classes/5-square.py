514a6c16c6cf8832bd11f1a1f497b3c5c4fc10ec
stefansilverio <494@holbertonschool.com>
2019-01-03 23:57:42 +0000

    print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        if isinstance(self.__size, int) is not True:
+            raise TypeError("size must be an integer")
+        else:
+            return self.__size**2
+
+    def my_print(self):
+        count = idx = 0
+        while self.__size > count:
+            idx = 0
+            while self.__size > idx:
+                print('#', end='')
+                idx += 1
+            print()
+            count += 1
+        if self.__size == 0:
+            print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
