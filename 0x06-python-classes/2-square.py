1ce0665ef324989ea954005cef239fd06fb6dbec
stefansilverio <494@holbertonschool.com>
2019-01-04 01:18:56 +0000

    initialize class with parameters

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
+        """Initialize a class.
+
+        Args:
+            param1: self
+            param2: size
+
+        Returns:
+            None
+        """
         if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
         elif size < 0:
