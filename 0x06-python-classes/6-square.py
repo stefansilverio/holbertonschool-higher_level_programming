9f68ef8d4f40c36e967f782dd3f057755df1856c
stefansilverio <494@holbertonschool.com>
2019-01-04 02:30:09 +0000

    print patterns and spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,6 +13,7 @@ class Square:
     @position.setter
     def position(self, value):
         if len(value) == 2 and isinstance(value, tuple) and \
+           isinstance(value[0], int) and isinstance(value[1], int) and \
            value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
