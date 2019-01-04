bf3ae39f19219cf81cc2dc0dc7870a4fa28682df
stefansilverio <494@holbertonschool.com>
2019-01-04 01:15:26 +0000

    check square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
+        """Initialize a class.
+        Args:
+            param1:
+            param2:
+
+        Returns:
+            none
+        """
         self.__size = size
