56488032d74f2b4f31a2380ebb656e5496159a94
stefansilverio <494@holbertonschool.com>
2019-01-09 03:20:04 +0000

    unit test folders

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_compare(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test_empty(self):
+        self.assertEqual(max_integer([None]), None)
+
+    def test_one(self):
+        self.assertEqual(max_integer([4]), 4)
+
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, 4]), 4)
+
+    def test_str(self):
+        self.assertEqual(max_integer(["stef", "cat", "is"]), "stef")
+
+    def test_two(self):
+        self.assertEqual(max_integer("stef"), "t")
+
+
+if __name__ == '__main__':
+    unittest.main()
