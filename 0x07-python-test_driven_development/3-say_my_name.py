d0f248cf49c9b56161a4cfe110711e3ebd7c6284
stefansilverio <494@holbertonschool.com>
2019-01-09 03:13:16 +0000

    print my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This function prints my name
+Returns:
+My name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints a name to stdout
+    Returns name
+    """
+    if isinstance(first_name, str) is not True:
+        raise TypeError("first_name must be a string")
+    elif isinstance(last_name, str) is not True:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {:1} {:2}".format(first_name, last_name))
