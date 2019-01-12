038fcda998b88e3927b84a80d98d4846dd1f48d5
stefansilverio <494@holbertonschool.com>
2019-01-12 03:59:05 +0000

    more edge cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,25 +15,30 @@ def matrix_divided(matrix, div):
 
     length = len(matrix[0])   # save length of matrix
 
-    if isinstance(matrix, list) is not True:
-        raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-
     if (isinstance(div, (int, float)) is not True):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for x in matrix:
-        if isinstance(x, list) is not True:
+    if isinstance(matrix, list) is not True:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
+    for row in matrix:
+        if isinstance(row, list) is not True:
             raise TypeError("matrix must be a matrix (list of lists) of \
  integers/floats")
-        for y in x:
-            if (isinstance(y, (int, float)) is not True):
+        if length != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_list = []
+
+        for column in row:
+            if (isinstance(column, (int, float)) is not True):
                 raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
-            if length != len(x):
-                raise TypeError("Each row of the matrix must have the same size")
-    new_matrix = [[round(i/div, 2) for i in j] for j in matrix]
+            new_list.append(round(column/div, 2))
+        new_matrix.append(new_list)
+
     return new_matrix
