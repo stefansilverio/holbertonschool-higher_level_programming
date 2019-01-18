40aa52344644a40f9d7167b5a80f6906d25f15d3
stefansilverio <494@holbertonschool.com>
2019-01-18 06:04:50 +0000

    create a student

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+create student
+Return:
+none
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize data
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        create dict
+        """
+        if isinstance(attrs, list):
+            for elem in attrs:
+                if isinstance(elem, str):
+                    continue
+                else:
+                    return self.__dict__
+
+            new_dict = {k: v for k, v in self.__dict__.items() if k in attrs}
+            return new_dict
+
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        re-initialize data
+        """
+        self.__dict__ = {k: v for k, v in json.items() if k
+                         in self.__dict__.keys()}
