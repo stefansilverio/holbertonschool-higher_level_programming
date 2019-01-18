8ffdcc1ff2e351106bef545c85c31c847c55dd4a
stefansilverio <494@holbertonschool.com>
2019-01-18 05:51:06 +0000

    serialize object

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+Return:
+Json rep
+of string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Return json rep of string
+    """
+    return json.dumps(my_obj)
