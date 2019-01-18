72f5710232049fa8595dde47b100896fc3c2921b
stefansilverio <494@holbertonschool.com>
2019-01-18 05:46:02 +0000

    print n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Read lines of a txt file
+Print n lines
+Return: Nothing
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    print n lines of a file
+    """
+    with open(filename) as f:
+        if nb_lines <= 0 or nb_lines >= len(f.readlines()):
+            print(f.read())
+
+        else:
+            with open(filename) as f:
+                for line in range(nb_lines):
+                    print(f.readline(), end="")
