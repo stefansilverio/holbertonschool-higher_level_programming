bacbdeda8bc8ddacec30f553847af1b688848cf1
stefansilverio <494@holbertonschool.com>
2019-01-21 00:04:50 +0000

    pascal's triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+"""
+this function builds
+pascals triangl
+Return: list of lists
+"""
+
 
 def pascal_triangle(n):
-    new_list = []
-    
+    """
+    Return: list of lists
+    """
+    sub_list = []
+    big_list = []
+    num = 11
+    ex = 0
+    for i in range(n):
+        result = 11**ex
+        string = str(result)
+        ex += 1
+        for c in string:
+            sub_list.append(c)
+        big_list.append(sub_list)
+        sub_list = []
+    return big_list
