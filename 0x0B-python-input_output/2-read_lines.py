4381c101a9c706b427ac2e96a9a978e3dca6c11c
stefansilverio <494@holbertonschool.com>
2019-01-21 00:40:32 +0000

    read from a file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,14 +10,13 @@ def read_lines(filename="", nb_lines=0):
     """
     print n lines of a file
     """
-    if (filename = ""):
-        return 0
 
     with open(filename) as f:
         if nb_lines <= 0 or nb_lines >= len(f.readlines()):
+            f.seek(0);
             print(f.read(), end="")
 
         else:
-            with open(filename) as f:
-                for line in range(nb_lines):
-                    print(f.readline(), end="")
+            f.seek(0);
+            for line in range(nb_lines):
+                print(f.readline(), end="")
