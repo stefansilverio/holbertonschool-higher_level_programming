a317e2d1965b720ec76ac23620ae8be8e7458a99
stefansilverio <494@holbertonschool.com>
2019-01-26 04:48:48 +0000

    class definitions

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import json
+
+class Base:
+    """base class for project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """count object"""
+        if (id):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert to json obj"""
+        if (len(list_dictionaries) == False or list_dictionaries == None):
+            return "[]"
+        else:
+            json_str = json.dumps(list_dictionaries)
+            return json_str
+
+    @staticmethod
+    def from_json_string(json_string):
+        if (len(json_string) == 0 or json_string == None):
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if (list_objs == None):
+            with open("{}.json".format(cls.__name__), "w") as f:
+                f.write(to_json_string([]))
+        else:
+            new = []
+            with open("{}.json".format(cls.__name__), "w") as f:
+                for obj in list_objs:
+                    new.append(obj.to_dictionary())
+                f.write(Base.to_json_string(new))
+
+    @classmethod
+    def create(cls, **dictionary):
+        if (cls.__name__ == "Square"):
+            dum = cls(4, 4, 4, 4)
+        if (cls.__name__ == "Rectangle"):
+            dum = cls(4, 4, 4, 4, 4)
+        dum.update(**dictionary)
+        return dum
+
+    @classmethod
+    def load_from_file(cls):
+        new = []
+        try:
+           with open("{}.json".format(cls.__name__), "r") as f:
+               dic = Base.from_json_string(f.read())
+               for i in dic:
+                   new.append(cls.create(**i))
+               return new
+        except Exception:
+            return []
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+"""
+Rectangle class inherits from base
+Return:
+Build rectangl
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    base class for project
+    """
+    def __init__(self, width, height, x = 0, y = 0, id=None):
+        """
+        build rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """set width"""
+        if (type(width) != int):
+            raise TypeError("width must be an integer")
+        if (width <= 0):
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """set height"""
+        if (type(height) != int):
+            raise TypeError("height must be an integer")
+        if (height <= 0):
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """get x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """set x"""
+        if (type(x) != int):
+            raise TypeError("x must be an integer")
+        if (x < 0):
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """get y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """set y"""
+        if (type(y) != int):
+            raise TypeError("y must be an integer")
+        if (y < 0):
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        """return area"""
+        return (self.width * self.height)
+
+    def display(self):
+        """display triangle as hashes"""
+        for nl in range(self.y):
+            print()
+        for h in range(self.height):
+            for space in range(self.x):
+                print(" ", end="")
+            for w in range(self.width):
+                print('#', end="")
+            print()
+
+    def __str__(self):
+        """customize print fn"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args, **kwargs):
+        """update rectangle"""
+        if (len(args) == False):
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.__dict__[k] = v
+                self.__dict__['_Rectangle__{}'.format(k)] = v
+
+        else:
+            l = ['width', 'height', 'x', 'y']
+            bound = len(args)
+            idx = 0
+            self.__dict__['id'] = args[0]
+            for elem in range(1, bound):
+                self.__dict__['_Rectangle__{}'.format(l[idx])] = args[elem]
+                idx += 1
+
+    def to_dictionary(self):
+        new = {}
+        new['x'] = self.x
+        new['y'] = self.y
+        new['id'] = self.id
+        new['height'] = self.height
+        new['width'] = self.width
+        return new
