d615c48027c226dc2ac8bef3e9ec84d72608578c
stefansilverio <494@holbertonschool.com>
2019-01-28 01:16:05 +0000

    added unittests

--- 0x0C-python-almost_a_circle/tests/test_rectangle_setters3.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle_setters3.py
@@ -6,38 +6,39 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 
+
 class TestBaseClass(unittest.TestCase):
 
     def test_e(self):
         with self.assertRaises(TypeError) as cm:
+            r1 = Rectangle("10", 2)
+        self.assertEqual('width must be an integer', str(cm.exception))
+        with self.assertRaises(TypeError) as cm:
             r1 = Rectangle(10, "2")
         self.assertEqual('height must be an integer', str(cm.exception))
-
-    def test_eq(self):
-        with self.assertRaises(ValueError) as cm:
-            r2 = Rectangle(10, 2)
-            r2.width = -10
-        self.assertEqual('width must be > 0', str(cm.exception))
-
-    def test_equ(self):
         with self.assertRaises(TypeError) as cm:
-            r3 = Rectangle("2")
-        self.assertEqual("__init__() missing 1 required positional argument: 'height'", str(cm.exception))
-
-    def test_equa(self):
+            r1 = Rectangle(10, 2, "3", 2)
+        self.assertEqual('x must be an integer', str(cm.exception))
         with self.assertRaises(TypeError) as cm:
-            r4 = Rectangle(10)
-        self.assertEqual("__init__() missing 1 required positional argument: 'height'", str(cm.exception))
-
-    def test_equal(self):
+            r1 = Rectangle(10, 2, 3, "2")
+        self.assertEqual('y must be an integer', str(cm.exception))
         with self.assertRaises(ValueError) as cm:
-            r5 = Rectangle(10, 2, -1, 2)
+            r1 = Rectangle(10, 2, -3, 2)
         self.assertEqual('x must be >= 0', str(cm.exception))
-
-    def test_equals(self):
         with self.assertRaises(ValueError) as cm:
-            r6 = Rectangle(10, 2, 3, -1)
+            r1 = Rectangle(10, 2, 3, -2)
         self.assertEqual('y must be >= 0', str(cm.exception))
-
-if __name__ == '__main__':
-    unittest.main()
+        with self.assertRaises(ValueError) as cm:
+            r1 = Rectangle(-10, 2, 3, 2)
+        self.assertEqual('width must be > 0', str(cm.exception))
+        with self.assertRaises(ValueError) as cm:
+            r1 = Rectangle(10, -2, 3, 2)
+        self.assertEqual('height must be > 0', str(cm.exception))
+        with self.assertRaises(TypeError) as cm:
+            r2 = Rectangle()
+        self.assertEqual("__init__() missing 2 required positional \
+arguments: 'width' and 'height'", str(cm.exception))
+        with self.assertRaises(TypeError) as cm:
+            r2 = Rectangle(2)
+        self.assertEqual("__init__() missing 1 required positional \
+argument: 'height'", str(cm.exception))
