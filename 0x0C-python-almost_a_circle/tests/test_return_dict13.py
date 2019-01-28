dcfdb3bf8a54b88932d56216fed80c56c69083c8
stefansilverio <494@holbertonschool.com>
2019-01-28 06:36:54 +0000

    added unittests

--- 0x0C-python-almost_a_circle/tests/test_return_dict13.py
+++ 0x0C-python-almost_a_circle/tests/test_return_dict13.py
@@ -5,10 +5,12 @@ return dictionary representation of rect
 import unittest
 from models.rectangle import Rectangle
 
+
 class TestBaseClass(unittest.TestCase):
     def test_e(self):
         r1 = Rectangle(10, 2, 1, 9)
         r1_dictionary = r1.to_dictionary()
+        self.assertEqual(isinstance(r1_dictionary, dict), True)
         self.assertEqual(str(r1_dictionary), "{'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}")
         self.assertEqual(str(type(r1_dictionary)), "<class 'dict'>")
         r2 = Rectangle(1, 1)
