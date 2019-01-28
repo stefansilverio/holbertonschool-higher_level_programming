b73720679b468b31ff9ca2bb501013fa21b73266
stefansilverio <494@holbertonschool.com>
2019-01-28 02:57:37 +0000

    added unittests

--- 0x0C-python-almost_a_circle/tests/test_rect_offset7.py
+++ 0x0C-python-almost_a_circle/tests/test_rect_offset7.py
@@ -8,6 +8,7 @@ from contextlib import redirect_stdout
 from models.base import Base
 from models.rectangle import Rectangle
 
+
 class TestBaseClass(unittest.TestCase):
     def test_e(self):
         r1 = Rectangle(2, 3, 2, 2)
@@ -21,3 +22,9 @@ class TestBaseClass(unittest.TestCase):
         with redirect_stdout(f):
             r2.display()
         self.assertEqual(f.getvalue(), " ###\n ###\n")
+
+        r3 = Rectangle(5, 2)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            r3.display()
+        self.assertEqual(f.getvalue(), "#####\n#####\n")
