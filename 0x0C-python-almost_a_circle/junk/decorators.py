1445c19a1fad7b86c01ddd3a6f8bb7c54bbba1d2
stefansilverio <494@holbertonschool.com>
2019-01-29 04:54:34 +0000

    delete old files

---
+++ 0x0C-python-almost_a_circle/junk/decorators.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def decorator_function(original_function):
+    def wrapper_function():
+        print('wrapper executed this before'.format(original_function.__name__))
+        return original_function()
+    return wrapper_function
+
+@decorator_function
+def display():
+    print('display function ran')
+
+#display = decorator_function(display)
+
+display()
