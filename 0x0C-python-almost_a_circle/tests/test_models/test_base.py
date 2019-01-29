8c42712027bf5d3aa3384d2a87b3fcec3375470f
stefansilverio <494@holbertonschool.com>
2019-01-29 03:46:43 +0000

    unittests added

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -28,11 +28,14 @@ positional arguments but 3 were given", str(cm.exception))
     def test_jrep(self):
         r1 = Rectangle(10, 7, 2, 8)
         dictionary = r1.to_dictionary()
+        self.assertEqual(print(dictionary), {'x': 2, 'width': 10,
+                                             'id': 1, 'height': 7, 'y': 8})
+        self.assertEqual(type(dictionary), "<class 'dict'>")
         json_dictionary = Base.to_json_string([dictionary])
         self.assertEqual(print(json_dictionary), [{"x": 2, "width":
                                                    10, "id": 1, "height": 7,
                                                    "y": 8}])
-        self.assertEqual((type(json_dictionary)), "<class 'str'>")
+        self.assertEqual(type(json_dictionary), "<class 'str'>")
         json_dictionary = Base.to_json_string([])
         self.asserEqual(print(json_dictionary), "[]")
 
@@ -41,9 +44,16 @@ positional arguments but 3 were given", str(cm.exception))
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[{"y": 8, "x": 2, "id": 1,
+                                            "width": 10, "height": 7},
+                                           {"y": 0, "x": 0, "id": 2,
+                                            "width": 2, "height": 4}]")
+            Rectangle.save_to_file([])
+            self.assertEqual(file.read(), "[]")
 
 #  #17
-    def test_equ(self):
+    def test_jsonst(self):
         list_input = [
             {'id': 89, 'width': 10, 'height': 4},
             {'id': 7, 'width': 1, 'height': 7}
