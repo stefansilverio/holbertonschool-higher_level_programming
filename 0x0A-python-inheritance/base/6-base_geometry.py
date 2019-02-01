4699358acca0bb5360aece8893bda0d7b551fe8d
stefansilverio <494@holbertonschool.com>
2019-02-01 17:55:55 +0000

    base class

---
+++ 0x0A-python-inheritance/base/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+raise exception
+Return:
+exception w/ message
+"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """raise exception
+        with message
+        """
+        raise Exception("area() is not implemented")
