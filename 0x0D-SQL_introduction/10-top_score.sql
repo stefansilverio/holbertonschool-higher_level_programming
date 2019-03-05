00f40519d5f96721ec08e7c268941ff5042c82ba
stefansilverio <494@holbertonschool.com>
2019-03-05 02:29:53 +0000

    list all records in second-table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- list records of second table
 -- display score and name
-SELECT `name`, `score` FROM second_table ORDER BY `score` DESC;
+SELECT `score`, `name` FROM second_table ORDER BY `score` DESC;
