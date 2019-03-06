ef4a61ff2b11995323e3982fd126785e2fd87e6a
stefansilverio <494@holbertonschool.com>
2019-03-06 05:46:08 +0000

    show all lists contained in database

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- show all shows in database
+-- return NULL if not exist
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id` FROM `tv_shows`
+        LEFT OUTER JOIN `tv_show_genres` ON `tv_shows`.`id`
+	 = `tv_show_genres`.`show_id` WHERE `tv_show_genres`.`show_id`
+	 IS NULL OR `tv_show_genres`.`show_id` IS NOT NULL;
