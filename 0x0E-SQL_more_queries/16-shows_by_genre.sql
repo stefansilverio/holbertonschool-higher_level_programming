5b372f6c75b57df04b2ab26bd2315725ac6e027f
stefansilverio <494@holbertonschool.com>
2019-03-07 03:24:12 +0000

    list all shows and genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- list all shows and all genres linked to show
+-- if show doesn't have genre display null
+SELECT `tv_shows`.`title`, `tv_genres`.`name` FROM `tv_shows`
+       LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` =
+       `tv_show_genres`.`show_id` LEFT JOIN `tv_genres` ON
+       `tv_show_genres`.`genre_id` = `tv_genres`.`id` ORDER BY
+       `tv_shows`.`title`, `tv_genres`.`name` ASC;
