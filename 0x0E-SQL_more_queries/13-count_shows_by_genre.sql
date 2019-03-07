eca2934e3d207e7ca265b006f274123eaac155d1
stefansilverio <494@holbertonschool.com>
2019-03-07 00:13:19 +0000

    genres w/ no links

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- tv shows that only contain
 -- sort in ascending order
-SELECT `tv_genres`.`name`, COUNT(*) number_of_shows FROM `tv_genres`
+SELECT `tv_genres`.`name` genre, COUNT(*) number_of_shows FROM `tv_genres`
        INNER JOIN `tv_show_genres` ON
        `tv_genres`.`id` = `tv_show_genres`.`genre_id` GROUP BY
        `tv_genres`.`name` ORDER BY `number_of_shows` DESC;
