db4f42b8ea04b1d52bbebf4d67abe0a8a1a6baed
stefansilverio <494@holbertonschool.com>
2019-03-29 17:58:48 +0000

    protect against injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""display matching arguments"""
+import MySQLdb
+import sqlalchemy
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    c=db.cursor()
+    c.execute("SELECT * FROM states WHERE `name` = %s ORDER BY states.id ASC", (sys.argv[4],))
+    states = c.fetchall()
+    for state in states:
+        print(state)
+    c.close()
+    db.close()
