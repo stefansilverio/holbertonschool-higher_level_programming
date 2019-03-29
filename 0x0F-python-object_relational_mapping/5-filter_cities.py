8e860b9f98046c4d89c4b2b341f39f3782aa15a8
stefansilverio <494@holbertonschool.com>
2019-03-29 17:59:34 +0000

    filter my cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""display matching arguments"""
+import MySQLdb
+import sqlalchemy
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    c=db.cursor()
+    c.execute("""SELECT `cities`.`name` FROM `cities` INNER JOIN
+    `states` ON `cities`.`state_id` = `states`.`id` WHERE
+    `states`.`name` = %s ORDER BY cities.id ASC""", (sys.argv[4],))
+    cities = c.fetchall()
+    count = 0
+    for city in cities:
+        print(''.join(city), end="")
+        count += 1
+        if count < len(cities):
+            print(", ", end="")
+    print()
+    c.close()
+    db.close()
