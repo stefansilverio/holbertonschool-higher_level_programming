50a7d62815bdb48daa59e4e3e097cd254eefe70b
stefansilverio <494@holbertonschool.com>
2019-03-30 01:50:43 +0000

    list all cities in db

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,10 +4,13 @@ import MySQLdb
 import sqlalchemy
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
-    c=db.cursor()
-    c.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    c = db.cursor()
+    c.execute("""SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+    FROM cities INNER JOIN states ON `cities`.`state_id` = `states`.`id`
+    ORDER BY cities.id ASC""")
     cities = c.fetchall()
     for city in cities:
         print(city)
