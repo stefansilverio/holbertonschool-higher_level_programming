10b0296b31c3e89bb29dab663621f8ccdfdec63d
stefansilverio <494@holbertonschool.com>
2019-03-30 02:27:10 +0000

    delete all state obj's w/ letter 'a'

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,10 +7,12 @@ import sqlalchemy
 import sys
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
+    for instance in session.query(State).filter(State.name.like
+                                                ("%a%")).order_by(State.id):
         session.delete(instance)
     session.commit()
     session.close()
