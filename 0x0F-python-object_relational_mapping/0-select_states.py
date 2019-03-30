6d27cc2ec19c4aa9338238a733a508972a95f9e9
stefansilverio <494@holbertonschool.com>
2019-03-30 01:21:12 +0000

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,12 @@ import MySQLdb
 import sqlalchemy
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-    c=db.cursor()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
     c.execute("""SELECT * FROM states ORDER BY 'states.id' ASC""")
     states = c.fetchall()
     for state in states:
-        print("", state)
-    # Close all cursors
+        print("{}".format(state))
     c.close()
-    # Close all databases
     db.close()
