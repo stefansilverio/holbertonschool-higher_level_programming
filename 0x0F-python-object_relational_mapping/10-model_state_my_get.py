916cfcfb27e52fa06886cb8237b62fbba33d03c3
stefansilverio <494@holbertonschool.com>
2019-03-30 02:15:45 +0000

    print state id

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,12 +7,13 @@ import sqlalchemy
 import sys
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
-        instance = session.query(State).filter(State.name==sys.argv[4]).one()
+        instance = session.query(State).filter(State.name == sys.argv[4]).one()
         print("{}".format(instance.id))
     except:
-        print("not found")
+        print("Not found")
     session.close()
