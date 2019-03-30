4b813fb2d930053a4d7ddc88fe70283d00daa44a
stefansilverio <494@holbertonschool.com>
2019-03-30 19:50:43 +0000

    join two tables

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""print all city obj's from hbtn_0e_14_usa"""
+from model_state import Base, State
+from model_city import Base, City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for s in session.query(City, State.name).join(State).\
+            order_by(City.id).all():
+        print("{}: ({}) {}".format(s[1], s[0].id, s[0].name))
+    session.close()
