d79fd0100e05dbe9116049d1360433910bebeb71
stefansilverio <494@holbertonschool.com>
2019-03-30 01:27:32 +0000

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,14 @@ import MySQLdb
 import sqlalchemy
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
-    c=db.cursor()
+    c = db.cursor()
     c.execute("""SELECT * FROM states WHERE ASCII(name)=ASCII('N')
     ORDER BY 'states.id' ASC""")
     states = c.fetchall()
     for state in states:
-        print("", state)
+        print("{}".format(state))
     c.close()
     db.close()
