edff79bd0a7ed440277511a0a7a16b7eff6583a4
stefansilverio <494@holbertonschool.com>
2019-03-30 01:33:46 +0000

    protect against injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,10 +4,12 @@ import MySQLdb
 import sqlalchemy
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
-    c=db.cursor()
-    c.execute("SELECT * FROM states WHERE `name` = %s ORDER BY states.id ASC", (sys.argv[4],))
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE `name` = %s ORDER BY states.id ASC",
+              (sys.argv[4],))
     states = c.fetchall()
     for state in states:
         print(state)
