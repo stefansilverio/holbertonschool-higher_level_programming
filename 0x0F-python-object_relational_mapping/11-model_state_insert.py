78d968b31bae8da4c19367abe47e15faf514ee3e
stefansilverio <494@holbertonschool.com>
2019-03-30 00:11:56 +0000

    add louisiana to the db

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""add state to database"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Iowa')
+    session.add(state)
+    session.commit()
+    print(state.id)
+    session.close()
