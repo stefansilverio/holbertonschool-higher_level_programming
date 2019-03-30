26d1fe9f9a443e6e1e77fcb2a9e5c49c99a756c6
stefansilverio <494@holbertonschool.com>
2019-03-30 01:08:39 +0000

    update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""change name of db object"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
+import sqlalchemy
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.id==2).first()
+    if instance:
+        instance.name = "New Mexico"
+        session.commit()
+    session.close()
