f70f92b49e72040b61776f6e3c0ed8018c4b0450
stefansilverio <494@holbertonschool.com>
2019-04-11 00:54:23 +0000

    display HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # display all HTTP methods
-curl -si -X OPTIONS $1 | grep "^\Allow:" | cut -d ":" -f 2 | sed -e
-'s/^[ \t]*//'
+curl -si -X OPTIONS $1 | grep '^\Allow:' | cut -d ':' -f 2 | sed -e 's/^[ \t]*//'
