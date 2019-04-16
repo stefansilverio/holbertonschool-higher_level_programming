70dab07487a76747ea0ee89ef67c857e4f75b36a
stefansilverio <494@holbertonschool.com>
2019-04-16 00:05:12 +0000

    display data about http request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# fetch a URL
+import urllib.request
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf-8')))
