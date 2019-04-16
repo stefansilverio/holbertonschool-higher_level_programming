e5c086b4dfdbba0bcf17a73ffd7fa2daa46937a8
stefansilverio <494@holbertonschool.com>
2019-04-16 02:05:33 +0000

    send request to URL, print value of header variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,5 +2,6 @@
 # display value of variable in response header
 import requests
 import sys
-r = requests.get(sys.argv[1])
-print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
