384a4390019e3cbc24c8c0af674f125fdde6ab07
stefansilverio <494@holbertonschool.com>
2019-04-16 06:02:52 +0000

    send letter to search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     else:
         q = sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", {'q': q})
-    if len(r.json()) > 0 and type(r.json()) is dict:
+    try:
         r = r.json()
-        print("{} {}".format([r.get('id')], r.get('name')))
-    elif len(r.json()) is 0:
-        print("No result")
-    else:
+        if len(r) > 0:
+            print("{} {}".format([r.get('id')], r.get('name')))
+        else:
+            print("No result")
+    except ValueError:
         print("Not a valid JSON")
