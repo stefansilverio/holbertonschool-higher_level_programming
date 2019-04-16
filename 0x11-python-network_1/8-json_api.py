2fdc228ba211ec53af2730e4453939a8239fb25a
stefansilverio <494@holbertonschool.com>
2019-04-16 03:41:26 +0000

    send POST request to server w/ letter as param

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,15 +2,16 @@
 # send letter as parameter to server as POST request
 import requests
 import sys
-if len(sys.argv) <= 1:
-    q = ""
-else:
-    q = sys.argv[1]
-r = requests.post("http://0.0.0.0:5000/search_user", {'q': q})
-if len(r.json()) > 0 and type(r.json()) is dict:
-    r = r.json()
-    print("{} {}".format([r.get('id')], r.get('name')))
-elif type(r.json()) is not dict:
-    print("Not a valid JSON")
-else:
-    print("No result")
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user", {'q': q})
+    if len(r.json()) > 0 and type(r.json()) is dict:
+        r = r.json()
+        print("{} {}".format([r.get('id')], r.get('name')))
+    elif type(r.json()) is not dict:
+        print("Not a valid JSON")
+    else:
+        print("No result")
