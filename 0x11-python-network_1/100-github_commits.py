df5681448f731733b7fff4d846e1f4d7f160cb05
stefansilverio <494@holbertonschool.com>
2019-04-22 06:32:56 +0000

    print last commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""print 10 most recent commits"""
+import requests
+import pprint
+from sys import argv
+if __name__ == "__main__":
+    me = argv[2] + "/" + argv[1]
+    url = "https://api.github.com/repos/" + me + "/commits"
+    r = requests.get(url).json()
+    for i in range(10):
+        print(r[i].get('sha'), end=": ")
+        print(r[i].get('commit').get('author').get('name'))
