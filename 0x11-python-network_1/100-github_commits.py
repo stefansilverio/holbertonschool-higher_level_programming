138d59b8655dd17d7992208c627117bf5f9531cd
stefansilverio <494@holbertonschool.com>
2019-04-22 06:43:04 +0000

    last 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,10 +2,14 @@
 """print 10 most recent commits"""
 import requests
 from sys import argv
+
 if __name__ == "__main__":
     me = argv[2] + "/" + argv[1]
     url = "https://api.github.com/repos/" + me + "/commits"
     r = requests.get(url).json()
-    for i in range(10):
-        print(r[i].get('sha'), end=": ")
-        print(r[i].get('commit').get('author').get('name'))
+    try:
+        for i in range(10):
+            print(r[i].get('sha'), end=": ")
+            print(r[i].get('commit').get('author').get('name'))
+    except Exception:
+        pass
