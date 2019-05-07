de0dd6986aa2ac540706325436b20a451f6ebfe3
stefansilverio <494@holbertonschool.com>
2019-05-07 22:38:14 +0000

    print three lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 /* loop through array */
 let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-arr.forEach(PrintFunction);
-function PrintFunction (item) {
-  console.log(item);
+let idx = 0;
+while (idx < arr.length) {
+  console.log(arr[idx]);
+  idx++;
 }
