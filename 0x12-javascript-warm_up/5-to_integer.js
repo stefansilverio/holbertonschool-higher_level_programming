6cd203eaf3bb882348ca2886b713bc7a28be0a47
stefansilverio <494@holbertonschool.com>
2019-05-07 22:34:17 +0000

    convert argument to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 /* convert arg to integer */
-if (isNaN(process.argv[2]) === true)
-  console.log('Not a number');
-else
-{
-  let num = parseInt(process.argv[2], 10)
+if (isNaN(process.argv[2]) === true) { console.log('Not a number'); } else {
+  let num = parseInt(process.argv[2], 10);
   console.log('My number: ' + num);
 }
