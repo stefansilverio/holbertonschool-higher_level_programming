4631bde6b8f930de94a5b8e83f65f71fcbf7a3dd
stefansilverio <494@holbertonschool.com>
2019-05-07 22:25:17 +0000

    print three lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* loop through array */
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+arr.forEach(PrintFunction);
+function PrintFunction (item) {
+  console.log(item);
+}
