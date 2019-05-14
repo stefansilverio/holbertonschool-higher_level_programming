5be9c767f097e6c6e45ad2d7077a8e01f6357c8f
stefansilverio <494@holbertonschool.com>
2019-05-14 19:09:35 +0000

    write string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // write a string to a file
 const fs = require('fs');
-let data = process.argv[3] + '\n';
+let data = process.argv[3];
 fs.writeFile(process.argv[2], data, 'utf8', (err) => {
   if (err) { console.log(err); }
 });
