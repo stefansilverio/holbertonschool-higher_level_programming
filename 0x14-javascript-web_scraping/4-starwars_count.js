f56e6813d5ca2ed7eabe709eb2543eda5029c87b
stefansilverio <494@holbertonschool.com>
2019-05-14 20:49:47 +0000

    print number of movies w/ character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// print number of movies w/ character
+const request = require('request');
+
+const options = {
+  url: 'http://swapi.co/api/films',
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'Mozilla/5.0'
+  }
+};
+
+request(options, function (err, res, body) {
+  if (err) { console.log('Problem reaching URL: ', err); }
+
+  let cnt = 0;
+  let idx = 0;
+  let json = JSON.parse(body);
+  let test = '18';
+  for (; idx < json['results'].length; idx++) {
+    let str = json['results'][idx]['characters'] + ',';
+    if (str.indexOf(test) >= 0) { cnt++; }
+  }
+  console.log(cnt);
+});
